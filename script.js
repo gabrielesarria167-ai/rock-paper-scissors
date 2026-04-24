72a2ab0ce1aea32d351221b02d106c32f63f7101
Gabriele Sarria <gabrielesarria167@gmail.com>
2026-04-24 20:49:19 +0200

    Add game functions, add gameButtons event listener
    I've finished the 2nd version of the rock paper scissors game, and it
    looks much more polished now.
    
    I added an event listener for the rock, paper and scissors buttons in
    the game window, and added several functions to play the game.
    
    The functions Ive added are the following:
    
    - playRound()
    - playMatch()
    - printRoundResult()
    - printMatchResult()

--- script.js
+++ script.js
@@ -10,11 +10,89 @@ function getComputerChoice() { // Function that gives the cpu a random choice be
     }
 }
 
-function setTotalRounds(event){
+function setTotalRounds(event) {
     event.preventDefault();
     totalRounds = input.value;
     formWindow.classList.toggle("hidden");
     gameWindow.classList.toggle("hidden");
+}
+
+function playRound(cpu, player) {
+    switch (true) {
+        case (cpu === player):
+            return "tie";
+        case (cpu === "rock" && player === "scissors"):
+        case (cpu === "paper" && player === "rock"):
+        case (cpu === "scissors" && player === "paper"):
+            return "lose";
+        default:
+            return "win";
+
+    }
+}
+
+function printRoundResult(wins, losses, ties, result){
+    switch (result) {
+        case "win":
+            gameResult.textContent = `You won the round! Score is: ${wins} wins, ${ties} ties and ${losses} losses.`;
+            return;
+        case "lose":
+            gameResult.textContent = `You lost this round... score is: ${wins} wins, ${ties} ties and ${losses} losses.`;
+            return;
+        default:
+            gameResult.textContent = `You tied this round. Score is: ${wins} wins, ${ties} ties and ${losses} losses.`;
+            return;
+    }
+}
+
+function printMatchResult(wins, losses, ties) {
+    if (wins + losses + ties === 1) {
+        switch (true) {
+            case (wins > losses):
+                gameResult.textContent = `You won the match!`;
+                return;
+            case (wins < losses):
+                gameResult.textContent = `You lost the match...`;
+                return;
+            default:
+                gameResult.textContent = `Its a tie!`;
+                return;
+        }
+    }
+    else {
+        switch (true) {
+            case (wins > losses):
+                gameResult.textContent = `You won the match! The final score was: ${wins} wins, ${ties} ties and ${losses} losses.`;
+                return;
+            case (wins < losses):
+                gameResult.textContent = `You lost the match... The final score was: ${wins} wins, ${ties} ties and ${losses} losses.`;
+                return;
+            default:
+                gameResult.textContent = `Its a tie! The final score was: ${wins} wins, ${ties} ties and ${losses} losses.`;
+                return;
+        }
+    }
+}
+
+function playMatch(rounds) {
+    if (rounds >= 1) {
+        switch (playRound(getComputerChoice(), playerChoice)) {
+            case "win":
+                wins++;
+                printRoundResult(wins, losses, ties, "win");
+                break;
+            case "lose":
+                losses++;
+                printRoundResult(wins, losses, ties, "lose");
+                break;
+            default:
+                ties++;
+                printRoundResult(wins, losses, ties);
+                break;
+        }
+    }
+    if (rounds <= 1) printMatchResult(wins, losses, ties);
+    return;
 }
 
 // Total rounds selection variables
@@ -22,9 +100,24 @@ function setTotalRounds(event){
 const formWindow = document.querySelector("#form");
 const confirmButton = document.querySelector("#confirm-button");
 const input = document.querySelector("#input");
-const gameWindow = document.querySelector("#gameWindow");
 
 let totalRounds = 0;
-input.max = 9, input.min = 1, input.step = 2, input.placeholder = "1";
+input.max = 21, input.min = 1, input.step = 2, input.placeholder = "1";
+
+// Match variables 
+
+const gameWindow = document.querySelector("#gameWindow");
+const gameButtons = document.querySelectorAll("#buttons > *");
+
+let gameResult = document.querySelector("#gameResult");
+let playerChoice = '';
+let wins = 0, losses = 0, ties = 0;
 
 confirmButton.addEventListener("click", (e) => setTotalRounds(e));
+gameButtons.forEach((item) => {
+    item.addEventListener("click", () => {
+        playerChoice = item.id;
+        playMatch(totalRounds);
+        totalRounds--;
+    });
+})
